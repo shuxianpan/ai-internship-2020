f013ca229dd26495f6ba48d1175916e0584998a4
zpan1787 <zpan1787@gmail.com>
2020-04-13 11:43:33 +0200

    Test
    The Stanza part. Not well formed.

--- main.py
+++ main.py
@@ -4,6 +4,16 @@ from pathlib import Path
 def init_nlp(lang, nlp_str="stanza"):
     # initialize the correct parser with the correct language
     # return initialised parser
+
+    import stanza
+    from stanza.utils.conll import CoNLL
+    stanza.download('en') # download English model
+    nlp = stanza.Pipeline('en', processors='tokenize,pos,lemma,depparse') # initialize English neural pipeline
+    doc = nlp("Barack Obama was born in Hawaii.") # run annotation over a sentence
+    dicts = doc.to_dict()
+    conll = CoNLL.convert_dict(dicts)
+
+
     return nlp
 
 

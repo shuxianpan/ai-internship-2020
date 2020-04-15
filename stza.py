de00f294f08d7524eb7615a92f3ba20fe6a8c06a
zpan1787 <zpan1787@gmail.com>
2020-04-16 01:42:09 +0200

    stanza as function

--- stza.py
+++ stza.py
@@ -2,10 +2,14 @@ import stanza
 
 from stanza.utils.conll import CoNLL
 
-stanza.download('en') # download English model
-nlp = stanza.Pipeline('en', processors='tokenize,pos,lemma,depparse') # initialize English neural pipeline
-doc = nlp("Barack Obama was born in Hawaii.") # run annotation over a sentence
+def _stanza_pipeline(lang,text):
+    nlp = stanza.Pipeline(lang, processors='tokenize,pos,lemma,depparse') # initialize English neural pipeline
+    doc = nlp(text) # run annotation over a sentence
+    dicts = doc.to_dict()
+    conll = CoNLL.convert_dict(dicts)
+    return conll
 
-dicts = doc.to_dict()
-conll = CoNLL.convert_dict(dicts)
-yield conll 
+#lang = 'en'
+#text = 'Barack Obama was born in Hawaii. He was the president of the United States.'
+#c = _stanza_pipeline(lang,text)
+#print(c)

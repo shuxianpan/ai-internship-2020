7c1f0de72d5fbf5aaed6c0083395533d8ccd707e
zpan1787 <zpan1787@gmail.com>
2020-04-16 01:46:50 +0200

    line_idx
    I don't know why I can't get line_idx. Is it about the doc.sents or self.include_headers?

--- udpipe.py
+++ udpipe.py
@@ -8,8 +8,8 @@ class SpacyUdpipe:
     def __init__(self, lang):
         self.h_out = None
         self.nlp = spacy_udpipe.load(lang)
-        self.Defaults = get_defaults(lang)
-        self.tagmap = self.Defaults.tag_map
+        self.tagmap = self.nlp.vocab.morphology.tag_map
+        text = "Barack Obama was born in Hawaii. He was the president of the United States."
         self.doc = self.nlp(text)
         self.is_tokenized = self.include_headers = False
         
@@ -17,26 +17,26 @@ class SpacyUdpipe:
         if not self.tagmap or tag not in self.tagmap:
             return '_'
         else:
-            feats = [f'{prop}={val}' for prop, val in self.tagmap[tag].items() if not Spacy2ConllParser._is_number(prop)]
+            feats = [f'{prop}={val}' for prop, val in self.tagmap[tag].items() if not SpacyUdpipe._is_number(prop)]
             if feats:
                 return '|'.join(feats)
             else:
                 return '_'
                 
     def _sentences_to_conllu(self, doc, line_idx):
-        for sentence in doc.sentences:
+        for sent in self.doc.sents:
             line_idx += 1
             parsed_sent = ''
 
             if self.include_headers:
                 parsed_sent = f'# sent_id = {str(line_idx)}\n'
-                parsed_sent += f'# text = {sentence.sentence}\n'
+                parsed_sent += f'# text = {sent.sent}\n'
 
-            for idx, token in enumerate(sentence, 1):
+            for idx, token in enumerate(sent, 1):
                 if token.dep_.lower().strip() == 'root':
                     head_idx = 0
                 else:
-                    head_idx = token.head.i + 1 - sentence[0].i
+                    head_idx = token.head.i + 1 - sent[0].i
 
                 line_tuple = (
                     idx,
@@ -57,19 +57,6 @@ class SpacyUdpipe:
 
             yield line_idx, parsed_sent
 
-
-    def get_defaults(self, lang) -> Language.Defaults:
-    #Get the language-specific defaults, if available in spaCy. This allows
-    #using lexical attribute getters that depend on static language data, e.g.
-    #Token.like_num, Token.is_stop, Doc.noun_chunks, etc.
-    #lang: ISO 639-1 language code or shorthand UDPipe model name.
-    #RETURNS: The language defaults.
-                try:
-                    lang_cls = get_lang_class(lang)
-                    return lang_cls.Defaults
-                except ImportError:
-                    return Language.Defaults
-
     @staticmethod       
     def _is_number(s):
         try:
@@ -86,8 +73,8 @@ model = SpacyUdpipe("en-ewt")
 
 lang = "en-ewt"
 
-text = "Barack Obama was born in Hawaii."
 
-conll = model._sentences_to_conllu(doc)
+
+conll = model._sentences_to_conllu(model.doc)
 
 print(conll)

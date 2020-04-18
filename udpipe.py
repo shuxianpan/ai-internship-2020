50ae39b97f5ab7a541d5a60fc5042f6e2e2c6374
zpan1787 <zpan1787@gmail.com>
2020-04-18 14:56:36 +0200

    Update udpipe.py

--- udpipe.py
+++ udpipe.py
@@ -17,7 +17,7 @@ class SpacyUdpipe:
         if not self.tagmap or tag not in self.tagmap:
             return '_'
         else:
-            feats = [f'{prop}={val}' for prop, val in self.tagmap[tag].items() if not SpacyUdpipe._is_number(prop)]
+            feats = [prop.replace("_","=") for prop in self.tagmap[tag].keys() if not SpacyUdpipe._is_number(prop)]
             if feats:
                 return '|'.join(feats)
             else:

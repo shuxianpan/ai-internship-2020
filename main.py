7e9e5dc1a0cbc419491db5bb8e5ec8fc6d7cfc0d
zpan1787 <zpan1787@gmail.com>
2020-06-03 10:34:55 +0200

    Update main.py

--- main.py
+++ main.py
@@ -13,37 +13,6 @@ class MainPipeline:
             self.model = Model.load(lang_or_model)
             self.nlp = Pipeline(self.model, 'tokenize','tag','parse', 'conllu')
 
-    # def _get_morphology(self, tag):
-
-    #     if not self.tagmap or tag not in self.tagmap:
-    #         return '_'
-    #     else:
-    #         feats = [prop.replace("_","=") for prop in self.tagmap[tag].keys() if not self._is_number(prop)]
-    #         if feats:
-    #             return '|'.join(feats)
-    #         else:
-    #             return '_'
-                
-    # def _sentences_to_conllu(self, doc):
-    # # based on the original codes from spacy_conll by Bram Vanroy:
-    # # https://github.com/BramVanroy/spacy_conll/blob/master/spacy_conll/SpacyConllParser.py
-    #     for sent in doc.sents:
-    #         parsed_sent = ''
-
-    #         for idx, token in enumerate(sent, 1):
-
-    #             if token.dep_.lower().strip() == 'root':
-    #                 head_idx = 0
-    #             else:
-    #                 head_idx = token.head.i + 1 - token.sent[0].i
-
-    #             strings = [str(idx), token.text, token.lemma_, token.pos_, token.tag_,\
-    #                            self._get_morphology(token.tag_), str(head_idx), token.dep_, '_', '_']
-
-    #             parsed_sent += '\t'.join(strings) + '\n'
-
-    #         yield parsed_sent
-
     def parse_to_conll(self, fin, nlp_str="stanza"):
         with open(fin, 'r', encoding="utf-8") as readinput:
             ri = readinput.read()
@@ -58,9 +27,7 @@ class MainPipeline:
         return conll
 
     def process_file(self, fin, nlp_str="stanza", out=None):
-    # read file `fin`, process line for line with the correct `nlp`, write CoNLL output to `out`
-    # return output file. It is either the given output file (if provided) or the same file path as the input file
-    # but with extension .conll. Use pathlib for Path manipulation.
+
         out = Path('output.conllu') if out is None else Path(out)
 
         with out.open("w", encoding="utf-8") as myfile:
@@ -76,16 +43,7 @@ class MainPipeline:
             elif nlp_str == "udpipe":
                 myfile.write(output)
 
-
         return out
-
-    # @staticmethod       
-    # def _is_number(s):
-    #     try:
-    #         float(s)
-    #         return True
-    #     except ValueError:
-    #         return False
 
 def main(fin, lang_or_model, out, nlp_str="stanza"):
 
